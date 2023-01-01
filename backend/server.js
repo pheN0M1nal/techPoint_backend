9eb152072a4bbea53f9c026f995bd96eb2a31449
phen0m1nal <nmnashrf@gmail.com>
2023-01-01 13:38:57 +0500

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -27,13 +27,6 @@ app.use(express.json())
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-}
-
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
@@ -53,6 +46,13 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 // 404
 app.use(notFound)
